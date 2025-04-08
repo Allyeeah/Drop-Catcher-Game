67695641aecb829a7c73f6aaa0e27a758e01f01f
Allyeeah <dbk1102@naver.com>
2025-04-09 01:52:20 +0900

    fix : basket 키보드 입력을 통해 이동으로 구현
    마우스 클릭해서 이동하는 거에서 키보드 w,a,s,d 누르면 이동하는 것으로 수정

--- Assets/Scripts/BasketController.cs
+++ Assets/Scripts/BasketController.cs
@@ -7,6 +7,7 @@ public class BasketController : MonoBehaviour
     AudioSource AS;
     public GameObject GM;
     ItemGenerator IG;
+    public float moveSpeed = 5f; // 이동 속도
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -15,25 +16,52 @@ public class BasketController : MonoBehaviour
         transform.position = Vector3.zero;
         AS = GetComponent<AudioSource>();
         IG = GM.GetComponent<ItemGenerator>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0)) 
-            // 마우스 좌클릭 0 , 휠클릭 1, 마우스 우클릭 2
-        {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hit;
-            if(Physics.Raycast(ray, out hit, Mathf.Infinity))
-            {  // out -> 참조 변수
-                // -1, 0, 1 
-                float x = Mathf.RoundToInt(hit.point.x); //좌표 반올림
-                float z = Mathf.RoundToInt(hit.point.z);
+        //키보드 입력으로 이동
+        float moveX = 0f;
+        float moveZ = 0f;
 
-                transform.position = new Vector3(x, 0, z);
-            }
-        }
+        // 좌우 이동 (A / D)
+        if (Input.GetKey(KeyCode.A))
+            moveX = -1f;
+        else if (Input.GetKey(KeyCode.D))
+            moveX = 1f;
+
+        // 상하 이동 (W / S)
+        if (Input.GetKey(KeyCode.W))
+            moveZ = 1f;
+        else if (Input.GetKey(KeyCode.S))
+            moveZ = -1f;
+
+        Vector3 move = new Vector3(moveX, 0, moveZ).normalized;
+        transform.position += move * moveSpeed * Time.deltaTime;
+
+        // 화면 밖 이동 방지 예시 (선택)
+        float clampedX = Mathf.Clamp(transform.position.x, -3f, 3f);
+        float clampedZ = Mathf.Clamp(transform.position.z, -3f, 3f);
+        transform.position = new Vector3(clampedX, 0, clampedZ);
+
+
+        /* 마우스 클릭으로 이동할 때
+         * if (Input.GetMouseButtonDown(0)) 
+             // 마우스 좌클릭 0 , 휠클릭 1, 마우스 우클릭 2
+         {
+             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+             RaycastHit hit;
+             if(Physics.Raycast(ray, out hit, Mathf.Infinity))
+             {  // out -> 참조 변수
+                 // -1, 0, 1 
+                 float x = Mathf.RoundToInt(hit.point.x); //좌표 반올림
+                 float z = Mathf.RoundToInt(hit.point.z);
+
+                 transform.position = new Vector3(x, 0, z);
+             }
+         }*/
     }
 
     private void OnTriggerEnter(Collider other)
